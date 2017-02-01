566ece0599cab839c6a9f9781e8c286de7aac581
Andreas Schlapbach <schlpbch@gmail.com>
2017-02-01 21:17:02 +0100

    Adding eye candy.

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -91,18 +91,132 @@ app.controller('AppCtrl', ['$scope', '$mdSidenav', '$mdDialog', '$http', functio
   };
 
   $scope.annuliereBuchung = function(event) {
-     $mdDialog.show(
-         $mdDialog.alert()
-         .parent(angular.element(document.querySelector('#popupContainer')))
-         .clickOutsideToClose(true)
-         .title('Buchung annulliert')
-         .textContent('Ihre Buchung wurde erfolgreich annulliert.')
-         .ariaLabel('Annullierungs Dialog')
-         .ok('Ok')
-         .targetEvent(event)
-     );
-   };
+    $mdDialog.show(
+      $mdDialog.alert()
+      .parent(angular.element(document.querySelector('#popupContainer')))
+      .clickOutsideToClose(true)
+      .title('Buchung annulliert')
+      .textContent('Ihre Buchung wurde erfolgreich annulliert.')
+      .ariaLabel('Annullierungs Dialog')
+      .ok('Ok')
+      .targetEvent(event)
+    );
+  };
 }]);
+
+
+app.controller('AbfahrtCtrl', ['$timeout', '$q', '$log', function($timeout, $q, $log) {
+  var self = this;
+
+  self.simulateQuery = false;
+  self.isDisabled = false;
+  self.states = loadAll();
+  self.querySearch = querySearch;
+  self.selectedItemChange = selectedItemChange;
+  self.searchTextChange = searchTextChange;
+  self.newState = newState;
+
+  function newState(state) {
+    alert("Sorry! You'll need to create a Constitution for " + state + " first!");
+  }
+
+  function querySearch(query) {
+    var results = query ? self.states.filter(createFilterFor(query)) : self.states,
+      deferred;
+    if (self.simulateQuery) {
+      deferred = $q.defer();
+      $timeout(function() {
+        deferred.resolve(results);
+      }, Math.random() * 1000, false);
+      return deferred.promise;
+    } else {
+      return results;
+    }
+  }
+
+  function searchTextChange(text) {
+    $log.info('Text changed to ' + text);
+  }
+
+  function selectedItemChange(item) {
+    $log.info('Item changed to ' + JSON.stringify(item));
+  }
+
+  function loadAll() {
+    var allStates = 'Bern, Thun ';
+    return allStates.split(/, +/g).map(function(state) {
+      return {
+        value: state.toLowerCase(),
+        display: state
+      };
+    });
+  }
+
+  function createFilterFor(query) {
+    var lowercaseQuery = angular.lowercase(query);
+
+    return function filterFn(state) {
+      return (state.value.indexOf(lowercaseQuery) === 0);
+    };
+  }
+}]);
+
+app.controller('AnkunftCtrl', ['$timeout', '$q', '$log', function($timeout, $q, $log) {
+  var self = this;
+
+  self.simulateQuery = false;
+  self.isDisabled = false;
+  self.states = loadAll();
+  self.querySearch = querySearch;
+  self.selectedItemChange = selectedItemChange;
+  self.searchTextChange = searchTextChange;
+  self.newState = newState;
+
+  function newState(state) {
+    alert("Sorry! You'll need to create a Constitution for " + state + " first!");
+  }
+
+  function querySearch(query) {
+    var results = query ? self.states.filter(createFilterFor(query)) : self.states,
+      deferred;
+    if (self.simulateQuery) {
+      deferred = $q.defer();
+      $timeout(function() {
+        deferred.resolve(results);
+      }, Math.random() * 1000, false);
+      return deferred.promise;
+    } else {
+      return results;
+    }
+  }
+
+  function searchTextChange(text) {
+    $log.info('Text changed to ' + text);
+  }
+
+  function selectedItemChange(item) {
+    $log.info('Item changed to ' + JSON.stringify(item));
+  }
+
+  function loadAll() {
+    var allStates = 'Bern, Thun ';
+    return allStates.split(/, +/g).map(function(state) {
+      return {
+        value: state.toLowerCase(),
+        display: state
+      };
+    });
+  }
+
+  function createFilterFor(query) {
+    var lowercaseQuery = angular.lowercase(query);
+
+    return function filterFn(state) {
+      return (state.value.indexOf(lowercaseQuery) === 0);
+    };
+  }
+}]);
+
 
 function DialogController($scope, $mdDialog) {
   $scope.hide = function() {
