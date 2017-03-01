1fe2d6d6f8a279615cb56e1754f01090eae789d1
Andreas Schlapbach <schlpbch@gmail.com>
2017-03-01 21:40:50 +0100

    ESliniting code base.

--- test/api/controllers/tickets.js
+++ test/api/controllers/tickets.js
@@ -4,20 +4,20 @@ var request = require('supertest')
 var server = require('../../../')
 
 // todo test failing, rest method not implemented
-describe('controllers', function () {
-    describe('Test issue tickets for Booking B1', function (done) {
-        describe('GET /booking/B1/tickets/', function (done) {
-            it('should not return an error', function (done) {
-                request(server)
-                    .get('/booking/B1/tickets/?type=pdf')
-                    .set('Accept', 'application/json')
-                    .expect('Content-Type', 'application/pdf')
-                    .expect(200)
-                    .end(function (err, res) {
-                        if (err) throw err
-                        else done()
-                    })
-            })
-        })
+describe('controllers', function() {
+  describe('Test issue tickets for Booking B1', function(done) {
+    describe('GET /booking/B1/tickets/', function(done) {
+      it('should not return an error', function(done) {
+        request(server)
+          .get('/booking/B1/tickets/?type=pdf')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', 'application/pdf')
+          .expect(200)
+          .end(function(err, res) {
+            if (err) throw err
+            else done()
+          })
+      })
     })
+  })
 })
