a3c1e890d592d4ec35ccf9775ae2a6f7ec7103c0
ue69708 <frank.pfleger@sbb.ch>
2017-03-02 17:06:55 +0100

    Naming of services improved

--- api/controllers/bookings.js
+++ api/controllers/bookings.js
@@ -1,27 +1,56 @@
 'use strict'
 
+const path = require('path')
+
 module.exports = {
-  getBookings
+    prebook,
+    book,
+    print
 }
 
-function getBookings(req, res) {
-  var booking = {
-    bookingId: 'B1',
-    description: 'Fahrt von Bern nach Thun am 14.01.2017 20:04.',
-    links: [{
-      rel: 'Billette zur Buchung als PDF holen',
-      href: 'bookings/B1/tickets/?type=pdf'
-    }, {
-      rel: 'Billette zur Buchung als Screenticket holen',
-      href: 'bookings/B1/tickets/?type=screen'
-    }, {
-      rel: 'Buchung annullieren',
-      href: 'bookings/B1/cancel'
-    }, {
-      rel: 'Buchung stornieren',
-      href: 'bookings/B1/refund'
-    }]
-  }
+function prebook(req, res) {
+    var prebooking = {
+        preBookId: 'P1',
+        description: 'Fahrt von Bern nach Thun am 14.01.2017 20:04 für 22 CHF',
+        links: [
+            {
+                rel: 'Billette zur Buchung holen',
+                href: 'bookings/P1/book'
+            }
+        ]
+    }
+    res.json(prebooking)
+}
 
-  res.json(booking)
+function book(req, res) {
+    var booking = {
+        bookingId: 'B1',
+        description: 'Fahrt von Bern nach Thun am 14.01.2017 20:04.',
+        links: [{
+            rel: 'Billette zur Buchung als PDF holen',
+            href: 'bookings/B1/print/?type=pdf'
+        }, {
+            rel: 'Billette zur Buchung als Screenticket holen',
+            href: 'bookings/B1/print/?type=screen'
+        }, {
+            rel: 'Buchung annullieren',
+            href: 'cancellations/B1/cancel'
+        }, {
+            rel: 'Buchung stornieren',
+            href: 'cancellations/B1/refund'
+        }]
+    }
+
+    res.json(booking)
+}
+
+function print(req, res) {
+    if (req.query.type === 'pdf') {
+        var options = {
+            root: path.join(__dirname, '/../../public/app')
+        }
+        res.sendFile('tickets/ticketB1.pdf', options)
+    } else {
+        res.sendStatus(415)
+    }
 }
--- api/controllers/locations.js
+++ api/controllers/locations.js
@@ -1,24 +1,24 @@
 'use strict'
 
 module.exports = {
-  getLocations
+    locations
 }
 
-function getLocations (req, res) {
-  var locationBernHB = {
-    id: '8507000',
-    name: 'Bern HB',
-    lon: 7.439122,
-    lat: 46.948825,
-    type: 'STATION'
-  }
-  var locationBernRBS = {
-    id: '8508050',
-    name: 'Bern RBS',
-    lat: 7.439122,
-    lon: 46.948825,
-    type: 'STATION'
-  }
-  var locations = [locationBernHB, locationBernRBS]
-  res.json(locations)
+function locations(req, res) {
+    var locationBernHB = {
+        id: '8507000',
+        name: 'Bern HB',
+        lon: 7.439122,
+        lat: 46.948825,
+        type: 'STATION'
+    }
+    var locationBernRBS = {
+        id: '8508050',
+        name: 'Bern RBS',
+        lat: 7.439122,
+        lon: 46.948825,
+        type: 'STATION'
+    }
+    var locations = [locationBernHB, locationBernRBS]
+    res.json(locations)
 }
