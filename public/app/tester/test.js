c7c10eff9de51ac74d63519c0e812ceb111ad700
kreuzerk <kevin.kreuzer90@icloud.com>
2017-03-15 21:26:26 +0100

    kk: Include browserify css

--- public/app/tester/test.js
+++ public/app/tester/test.js
@@ -2,6 +2,7 @@
  * Created by kevinkreuzer on 15.03.17.
  */
 var controller = require('./tester.controller');
+require('./test.css');
 
 module.exports = angular.module('test', [])
   .component('test', {controller: controller, templateUrl: 'app/tester/test.html'});
