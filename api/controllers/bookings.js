ba51873d69ed8c56cd30e9272006625a52629842
ue69708 <frank.pfleger@sbb.ch>
2017-03-28 14:55:31 +0200

    FSM API alignment

--- api/controllers/bookings.js
+++ api/controllers/bookings.js
@@ -3,11 +3,31 @@
 const path = require('path')
 
 module.exports = {
-    fulfill,
+    confirm,
+    fulfil,
     cancel
 }
 
-function fulfill(req, res) {
+function confirm(req, res) {
+    var booking = {
+        bookingId: 'B1',
+        description: 'Fahrt von Bern nach Thun am 14.01.2017 20:04.',
+        links: [{
+            rel: 'Billette zur Buchung als PDF holen',
+            href: 'bookings/fulfil/B1?type=pdf'
+        }, {
+            rel: 'Billette zur Buchung als Screenticket holen',
+            href: 'bookings/fulfil/B1?type=screen'
+        }, {
+            rel: 'Buchung annullieren',
+            href: 'bookings/cancel/B1'
+        }]
+    }
+
+    res.json(booking)
+}
+
+function fulfil(req, res) {
     if (req.query.type === 'pdf') {
         var options = {
             root: path.join(__dirname, '/../../public/app')
