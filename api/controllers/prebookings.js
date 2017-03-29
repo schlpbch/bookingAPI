09da5bb2e5368d765a56be313a43b264a43c423c
ue69708 <frank.pfleger@sbb.ch>
2017-03-29 15:21:51 +0200

    SSC API alignment

--- api/controllers/prebookings.js
+++ api/controllers/prebookings.js
@@ -8,7 +8,7 @@ module.exports = {
 
 function prebook(req, res) {
     var prebooking = {
-        preBookId: 2001,
+        preBookId: '2001',
         description: 'Fahrt von Bern nach Thun am 14.01.2017 20:04 für 22 CHF',
         links: [
             {
