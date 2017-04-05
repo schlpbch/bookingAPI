8d93216e10a3a4515dfee3e4f7b851b4f6c6a716
codepfleger <mail@codepfleger.de>
2017-04-05 10:19:46 +0200

    Neues Link-Pattern + Bookings-Backend

--- api/controllers/bookings.js
+++ api/controllers/bookings.js
@@ -12,16 +12,17 @@ function confirm (req, res) {
   var booking = {
     bookingId: '3001',
     description: 'Fahrt von Bern nach Thun am 14.01.2017 20:04.',
-    _links: [{
-      rel: 'Billette zur Buchung als PDF holen',
-      href: 'bookings/3001/fulfil?type=pdf'
-    }, {
-      rel: 'Billette zur Buchung als Screenticket holen',
-      href: 'bookings/3001/fulfil?type=screen'
-    }, {
-      rel: 'Buchung annullieren',
-      href: 'bookings/3001/cancel'
-    }]
+    _links: {
+        'self': {
+            href: 'bookings/3001'
+        }, 'fulfil': {
+            title: 'Billette zur Buchung als PDF holen', href: 'bookings/3001/fulfil?type=pdf'
+        }, 'book': {
+            title: 'Billette zur Buchung als Screenticket holen', href: 'bookings/3001/fulfil?type=screen'
+        }, 'cancel': {
+            title: 'Buchung annullieren', href: 'bookings/3001/cancel'
+        }
+    }
   }
 
   res.json(booking)
