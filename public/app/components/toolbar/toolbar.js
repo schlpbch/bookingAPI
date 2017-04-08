91c6e64615e2de5673e77674f1165e35d36eba6b
kreuzerk <kevin.kreuzer90@icloud.com>
2017-04-08 12:54:27 +0200

    kk: Fix Navbar toggle on mobile

--- public/app/components/toolbar/toolbar.js
+++ public/app/components/toolbar/toolbar.js
@@ -1,11 +1,13 @@
 /**
  * Created by kevinkreuzer on 16.03.17.
  */
+import toolbarController from './toolbar.controller';
 
 let moduleName = 'toolbar';
 
 angular.module(moduleName, [])
-  .component('apiToolbar',{
-    templateUrl: 'components/toolbar/toolbar.html'
-  });
+    .component('apiToolbar', {
+        controller: toolbarController,
+        templateUrl: 'components/toolbar/toolbar.html'
+    });
 export default moduleName;
