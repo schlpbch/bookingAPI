d8249d4c95a607170b770c3437d29b166629f1ac
kreuzerk <kevin.kreuzer90@icloud.com>
2017-04-12 08:11:08 +0200

    kk: Customize dialog

--- public/app/components/toolbar/settings/settings-dialog.controller.js
+++ public/app/components/toolbar/settings/settings-dialog.controller.js
@@ -1,6 +1,21 @@
 /**
  * Created by kevinkreuzer on 12.04.17.
  */
-export default class SettingsDialogController{
+export default class SettingsDialogController {
 
+    constructor($mdDialog) {
+        this.$mdDialog = $mdDialog;
+    }
+
+    hide() {
+        this.$mdDialog.hide();
+    };
+
+    cancel() {
+        this.mdDialog.cancel();
+    };
+
+    answer(answer) {
+        this.mdDialog.hide(answer);
+    };
 }
