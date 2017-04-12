114c471dbd431128ba3430fabd25cdddc7e9c7a6
kreuzerk <kevin.kreuzer90@icloud.com>
2017-04-12 08:03:21 +0200

    kk: Open popup

---
+++ public/app/components/toolbar/settings/settings-dialog.controller.js
@@ -0,0 +1,6 @@
+/**
+ * Created by kevinkreuzer on 12.04.17.
+ */
+export default class SettingsDialogController{
+
+}
--- public/app/components/toolbar/toolbar.controller.js
+++ public/app/components/toolbar/toolbar.controller.js
@@ -1,12 +1,31 @@
 /**
  * Created by kevinkreuzer on 08.04.17.
  */
+import SettingsDialogController from './settings/settings-dialog.controller'
+
 export default class ToolbarController {
-    constructor($mdSidenav) {
+    constructor($mdSidenav, $mdDialog) {
         this.$mdSidenav = $mdSidenav
+        this.$mdDialog = $mdDialog
     }
 
     toggleSidenav(menuId) {
         this.$mdSidenav(menuId).toggle()
     }
+
+    showAdvanced(ev) {
+        this.$mdDialog.show({
+            controller: SettingsDialogController,
+            templateUrl: 'components/toolbar/settings/settings-dialog.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose:true,
+            fullscreen: false
+        })
+            .then(function(answer) {
+                console.log('Handle answer', answer)
+            }, function() {
+                console.log('Handle cancel')
+            });
+    };
 }
