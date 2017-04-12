9d81acabc4a3a68b69451ab632f2395226551ab2
kreuzerk <kevin.kreuzer90@icloud.com>
2017-04-12 07:40:34 +0200

    kk: Add menu

--- public/app/components/toolbar/toolbar.controller.js
+++ public/app/components/toolbar/toolbar.controller.js
@@ -2,11 +2,11 @@
  * Created by kevinkreuzer on 08.04.17.
  */
 export default class ToolbarController {
-  constructor ($mdSidenav) {
-    this.$mdSidenav = $mdSidenav
-  }
+    constructor($mdSidenav) {
+        this.$mdSidenav = $mdSidenav
+    }
 
-  toggleSidenav (menuId) {
-    this.$mdSidenav(menuId).toggle()
-  }
+    toggleSidenav(menuId) {
+        this.$mdSidenav(menuId).toggle()
+    }
 }
