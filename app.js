969c08f3188e3c5db8d2549809318b3fd843d934
Andreas Schlapbach <schlpbch@gmail.com>
2017-04-17 19:03:42 +0200

    Add morgan support (#62)

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var SwaggerUi = require('swagger-tools/middleware/swagger-ui')
 var express = require('express')
 var path = require('path')
 var request = require('request')
+var morgan = require('morgan')
 var app = express()
 
 module.exports = app // for testing
@@ -23,31 +24,32 @@ SwaggerExpress.create(config, function (err, swaggerExpress) {
     throw err
   }
 
-    // install middleware
+  // install middleware
 
   app.use(SwaggerUi(swaggerExpress.runner.swagger))
   swaggerExpress.register(app)
 
   var port = process.env.PORT || 8080
 
-    // set static files location used for requests that our frontend will make
+  // set static files location used for requests that our frontend will make
+  app.use(morgan('dev'))
   app.use(express.static(path.join(__dirname, '/public')))
 
   app.get('/redirect_api/locations*', function (req, res) {
-      request(`${backendReise}/api/locations${req.params[0]}${req._parsedUrl.search ? req._parsedUrl.search : ''}`).pipe(res);
-  });
+    request(`${backendReise}/api/locations${req.params[0]}${req._parsedUrl.search ? req._parsedUrl.search : ''}`).pipe(res)
+  })
   app.get('/redirect_api/trips*', function (req, res) {
-      request(`${backendReise}/api/trips${req.params[0]}${req._parsedUrl.search ? req._parsedUrl.search : ''}`).pipe(res);
-  });
+    request(`${backendReise}/api/trips${req.params[0]}${req._parsedUrl.search ? req._parsedUrl.search : ''}`).pipe(res)
+  })
   app.get('/redirect_api/offers*', function (req, res) {
-      request(`${backendReise}/api/offers${req.params[0]}${req._parsedUrl.search ? req._parsedUrl.search : ''}`).pipe(res);
-  });
+    request(`${backendReise}/api/offers${req.params[0]}${req._parsedUrl.search ? req._parsedUrl.search : ''}`).pipe(res)
+  })
   app.get('/redirect_api/prebookings*', function (req, res) {
-      request(`${backendOrch}/api/prebookings${req.params[0]}${req._parsedUrl.search ? req._parsedUrl.search : ''}`).pipe(res);
-  });
+    request(`${backendOrch}/api/prebookings${req.params[0]}${req._parsedUrl.search ? req._parsedUrl.search : ''}`).pipe(res)
+  })
   app.get('/redirect_api/bookings*', function (req, res) {
-      request(`${backendOrch}/api/bookings${req.params[0]}${req._parsedUrl.search ? req._parsedUrl.search : ''}`).pipe(res);
-  });
+    request(`${backendOrch}/api/bookings${req.params[0]}${req._parsedUrl.search ? req._parsedUrl.search : ''}`).pipe(res)
+  })
 
   app.listen(port)
 
