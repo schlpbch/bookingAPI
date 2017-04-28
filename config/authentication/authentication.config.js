9f729c60f3699446d9b243adc14650ed4fa52df8
kreuzerk <kevin.kreuzer90@icloud.com>
2017-04-28 17:42:48 +0200

    kk: Write token to process env

--- config/authentication/authentication.config.js
+++ config/authentication/authentication.config.js
@@ -1,13 +1,14 @@
 /**
  * Created by kevinkreuzer on 24.04.17.
  */
+
 let github = {
-    clientSecret: '9d3893393de2dfd406ab091b9b0a97d153550e71',
+    clientSecret: process.env.GITHUB_SECRET,
     accessTokenUrl: 'https://github.com/login/oauth/access_token',
     userApiUrl: 'https://api.github.com/user'
 }
 
 module.exports = {
-    TOKEN_SECRECT: 'awesomeBookingSecret',
+    TOKEN_SECRECT: process.env.TOKEN_SECRET,
     github
 }
