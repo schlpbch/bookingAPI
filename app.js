53d9b5ae3f4291f6214c61fad4216718c08cb506
kreuzerk <kevin.kreuzer90@icloud.com>
2017-05-05 15:15:08 +0200

    kk: Extract reverse proxy to a seperate file

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 'use strict'
-
 var SwaggerExpress = require('swagger-express-mw')
 var SwaggerUi = require('swagger-tools/middleware/swagger-ui')
 var express = require('express')
 var path = require('path')
-var request = require('request')
+var createReverseProxy = require('./api/proxy/reverse.proxy')
 var morgan = require('morgan')
 var app = express()
 
 module.exports = app // for testing
 
 var config = {
-  appRoot: __dirname // required config
+    appRoot: __dirname // required config
 }
 
 var backendReise = process.env.BACKEND_REISE || 'http://localhost:8080'
@@ -20,40 +19,22 @@ var backendOrch = process.env.BACKEND_ORCH || 'http://localhost:8080'
 console.log('Backend Orch: ', backendOrch)
 
 SwaggerExpress.create(config, function (err, swaggerExpress) {
-  if (err) {
-    throw err
-  }
+    if (err) {
+        throw err
+    }
+    // install middleware
+    app.use(SwaggerUi(swaggerExpress.runner.swagger))
+    swaggerExpress.register(app)
 
-  // install middleware
+    var port = process.env.PORT || 8080
 
-  app.use(SwaggerUi(swaggerExpress.runner.swagger))
-  swaggerExpress.register(app)
+    // set static files location used for requests that our frontend will make
+    app.use(morgan('dev'))
+    app.use(express.static(path.join(__dirname, '/public')))
+    createReverseProxy(app, backendReise)
+    app.listen(port)
 
-  var port = process.env.PORT || 8080
-
-  // set static files location used for requests that our frontend will make
-  app.use(morgan('dev'))
-  app.use(express.static(path.join(__dirname, '/public')))
-
-  app.get('/redirect_api/locations*', function (req, res) {
-    request(`${backendReise}/api/locations${req.params[0]}${req._parsedUrl.search ? req._parsedUrl.search : ''}`).pipe(res)
-  })
-  app.get('/redirect_api/trips*', function (req, res) {
-    request(`${backendReise}/api/trips${req.params[0]}${req._parsedUrl.search ? req._parsedUrl.search : ''}`).pipe(res)
-  })
-  app.get('/redirect_api/offers*', function (req, res) {
-    request(`${backendReise}/api/offers${req.params[0]}${req._parsedUrl.search ? req._parsedUrl.search : ''}`).pipe(res)
-  })
-  app.get('/redirect_api/prebookings*', function (req, res) {
-    request(`${backendOrch}/api/prebookings${req.params[0]}${req._parsedUrl.search ? req._parsedUrl.search : ''}`).pipe(res)
-  })
-  app.get('/redirect_api/bookings*', function (req, res) {
-    request(`${backendOrch}/api/bookings${req.params[0]}${req._parsedUrl.search ? req._parsedUrl.search : ''}`).pipe(res)
-  })
-
-  app.listen(port)
-
-  if (swaggerExpress.runner.swagger.paths['/offers']) {
-    console.log('try this: http://localhost:' + port + '/app')
-  }
+    if (swaggerExpress.runner.swagger.paths['/offers']) {
+        console.log('try this: http://localhost:' + port + '/app')
+    }
 })
