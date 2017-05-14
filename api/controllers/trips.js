ac997fca98d449a8940997f755ed23d3ddb8f0ea
Andreas Schlapbach <schlpbch@gmail.com>
2017-05-14 22:28:43 +0200

    Translate HAL titles

--- api/controllers/trips.js
+++ api/controllers/trips.js
@@ -22,17 +22,21 @@ function trips (req, res) {
       }
     }],
     _links: {
-        'self': {
-            href: 'trips/trip-rec-context1'
-        }, 'prices': {
-            title: 'Request the cheapest price for a trip.', href: 'api/prices/trip-rec-context1'
-        }, 'offers': {
-            title: 'Request offers for a trip.', href: 'api/offers?originId=8507000&destinationId=8503000&date=2017-05-01'
-        }
+      'self': {
+        href: 'trips/trip-rec-context1'
+      },
+      'prices': {
+        title: 'Request the cheapest price for a trip.',
+        href: 'api/prices/trip-rec-context1'
+      },
+      'offers': {
+        title: 'Request offers for a trip.',
+        href: 'api/offers?originId=8507000&destinationId=8503000&date=2017-05-01'
+      }
     }
   }
 
-    // first leg of second trip
+  // first leg of second trip
 
   var trip2 = {
     tripId: 'trip-rec-context2',
@@ -51,13 +55,17 @@ function trips (req, res) {
       }
     }],
     _links: {
-        'self': {
-            href: 'trips/trip-rec-context2'
-        }, 'prices': {
-            title: 'Request the cheapest price for a trip.', href: 'api/prices/trip-rec-context2'
-        }, 'offers': {
-            title: 'Request offers for a trip.', href: 'api/offers?originId=8507000&destinationId=8503000&date=2017-05-02'
-        }
+      'self': {
+        href: 'trips/trip-rec-context2'
+      },
+      'prices': {
+        title: 'Request the cheapest price for a trip.',
+        href: 'api/prices/trip-rec-context2'
+      },
+      'offers': {
+        title: 'Request offers for a trip.',
+        href: 'api/offers?originId=8507000&destinationId=8503000&date=2017-05-02'
+      }
     }
   }
 
