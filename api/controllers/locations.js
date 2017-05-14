eed4d4c60f3592e60a8bdc26745ba86e00e36f0e
Andreas Schlapbach <schlpbch@gmail.com>
2017-05-14 13:47:58 +0200

    Adding Thun to available locations

--- api/controllers/locations.js
+++ api/controllers/locations.js
@@ -5,13 +5,6 @@ module.exports = {
 }
 
 function locations (req, res) {
-  var locationZuerichHB = {
-      id: '8503000',
-      name: 'Zürich HB',
-      lon: 7.439122,
-      lat: 46.948825,
-      type: 'STATION'
-  }
   var locationBernHB = {
     id: '8507000',
     name: 'Bern HB',
@@ -26,6 +19,20 @@ function locations (req, res) {
     lon: 46.948825,
     type: 'STATION'
   }
-  var locations = [locationZuerichHB, locationBernHB, locationBernRBS]
+  var locationThunHB = {
+    id: '8507100',
+    name: 'Thun HB',
+    lat: 7.629594,
+    lon: 46.754853,
+    type: 'STATION'
+  }
+  var locationZuerichHB = {
+    id: '8503000',
+    name: 'Zürich HB',
+    lon: 8.540192,
+    lat: 47.378176,
+    type: 'STATION'
+  }
+  var locations = [locationBernHB, locationBernRBS, locationThunHB, locationZuerichHB]
   res.json(locations)
 }
