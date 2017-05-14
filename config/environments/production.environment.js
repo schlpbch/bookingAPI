667f71c0652af5ac6310843e663bd973b6c6e025
kreuzerk <kevin.kreuzer90@icloud.com>
2017-05-14 22:33:35 +0200

    kk: Fix prod build

--- config/environments/production.environment.js
+++ config/environments/production.environment.js
@@ -2,8 +2,8 @@
  * Created by kevinkreuzer on 12.05.17.
  */
 module.exports = {
-    backendReise: process.env.BACKEND_REISE,
-    backendOrch: process.env.BACKEND_ORCH,
-    appPort: process.env.PORT,
+    backendReise: process.env.BACKEND_REISE || 'http://localhost:8080',
+    backendOrch: process.env.BACKEND_ORCH || 'http://localhost:8080',
+    appPort: process.env.PORT || 8080,
     morganFormat: 'common'
 }
