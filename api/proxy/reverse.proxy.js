287b4dde0e321c9344a74b7f1acf89900b16c074
kreuzerk <kevin.kreuzer90@icloud.com>
2017-05-19 14:09:30 +0200

    kk: Use Proxy - pipe

--- api/proxy/reverse.proxy.js
+++ api/proxy/reverse.proxy.js
@@ -6,38 +6,43 @@ const appendQuery = require('append-query')
 const request = require('request')
 const createReverseProxy = (app, environmentConfiguration) => {
 
-    const proxyAPIRequest = (uri, req, res) => {
+    const proxyAPIRequest = (baseURL, path, clientRequest, clientResponse) => {
+        let headers = clientRequest.headers
+        let url = appendQuery(`${baseURL}/api/${path}`, clientRequest.query)
+        //TODO: Use Certificate solution instead of rejectUnauthorized: false
+        console.log('New Proxy: Calling ', url)
+        request(url, {headers, rejectUnauthorized: false}).pipe(clientResponse)
+    }
+
+    const proxyAPIRequestOld = (uri, req, res) => {
         let backendReise = environmentConfiguration.backendReise
+        console.log('Calling', `${backendReise}/api/${uri}${req.params[0]}${req._parsedUrl.search ? req._parsedUrl.search : ''}`)
         request(`${backendReise}/api/${uri}${req.params[0]}${req._parsedUrl.search ? req._parsedUrl.search : ''}`).pipe(res)
     }
-    app.get('/redirect_api/locations*', function (req, res) {
-        proxyAPIRequest('locations', req, res)
+
+    const proxyReiseRequest = (path, clientRequest, clientResponse) => {
+        proxyAPIRequest(environmentConfiguration.backendReise, path, clientRequest, clientResponse)
+    }
+
+    app.get('/redirect_api/locations*', function (clientRequest, clientResponse) {
+        proxyReiseRequest('locations', clientRequest, clientResponse)
+        //proxyAPIRequestOld('locations', clientRequest, clientResponse)
     })
-    app.get('/redirect_api/trips*', function (req, res) {
-        proxyAPIRequest('trips', req, res)
+
+    app.get('/redirect_api/trips*', function (clientRequest, clientResponse) {
+        proxyReiseRequest('trips', clientRequest, clientResponse)
     })
+
     app.get('/redirect_api/offers*', function (clientRequest, clientResponse) {
-        let headers = clientRequest.headers
-        let basicAuthURL = 'https://zvs-api-test-ws.sbb.ch/api/offers'
-        let url = appendQuery(basicAuthURL, clientRequest.query)
-        console.log('Calling the following url', url)
-        //TODO: Use Certificate solution instead of rejectUnauthorized: false
-        request(url, {headers, rejectUnauthorized: false}, function (request, response) {
-            if (response.statusCode === 200) {
-                clientResponse.send(response.body)
-            }
-            else {
-                clientResponse.status(500).send({
-                    message: 'Ouupsi'
-                });
-            }
-        })
+        proxyReiseRequest('offers', clientRequest, clientResponse)
     })
-    app.get('/redirect_api/prebookings*', function (req, res) {
-        proxyAPIRequest('prebookings', req, res)
+
+    app.get('/redirect_api/prebookings*', function (clientRequest, clientResponse) {
+        //proxyReiseRequest('prebookings', clientRequest, clientResponse)
+        proxyAPIRequestOld('prebookings', clientRequest, clientResponse)
     })
-    app.get('/redirect_api/bookings*', function (req, res) {
-        proxyAPIRequest('bookings', req, res)
+    app.get('/redirect_api/bookings*', function (clientRequest, clientResponse) {
+        proxyReiseRequest('bookings', clientRequest, clientResponse)
     })
 
     app.get('/basicAuth/login', function (clientRequest, clientResponse) {
