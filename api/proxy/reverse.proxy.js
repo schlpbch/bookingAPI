c2c49fd114f579dd32985ce80f233f5d30b8d6f6
kreuzerk <kevin.kreuzer90@icloud.com>
2017-05-19 20:24:07 +0200

    kk: Call WSG with prod file

--- api/proxy/reverse.proxy.js
+++ api/proxy/reverse.proxy.js
@@ -6,35 +6,15 @@ const request = require('request')
 const REDIRECT_API = '/redirect_api/'
 const createReverseProxy = (app, environmentConfiguration) => {
 
-    const proxyAPIRequest = (baseURL, path, clientRequest, clientResponse) => {
+    const proxyAPIRequest = (clientRequest, clientResponse) => {
         let headers = clientRequest.headers
         let url = clientRequest.url.replace(REDIRECT_API, `${environmentConfiguration.backendReise}/api/`)
         //TODO: Use Certificate solution instead of rejectUnauthorized: false
         request(url, {headers, rejectUnauthorized: false}).pipe(clientResponse)
     }
 
-    const proxyReiseRequest = (path, clientRequest, clientResponse) => {
-        proxyAPIRequest(environmentConfiguration.backendReise, path, clientRequest, clientResponse)
-    }
-
-    app.get('/redirect_api/locations*', function (clientRequest, clientResponse) {
-        proxyReiseRequest('locations', clientRequest, clientResponse)
-    })
-
-    app.get('/redirect_api/trips*', function (clientRequest, clientResponse) {
-        proxyReiseRequest('trips', clientRequest, clientResponse)
-    })
-
-    app.get('/redirect_api/offers*', function (clientRequest, clientResponse) {
-        proxyReiseRequest('offers', clientRequest, clientResponse)
-    })
-
-    app.get('/redirect_api/prebookings*', function (clientRequest, clientResponse) {
-        proxyReiseRequest('prebookings', clientRequest, clientResponse)
-    })
-
-    app.get('/redirect_api/bookings*', function (clientRequest, clientResponse) {
-        proxyReiseRequest('bookings', clientRequest, clientResponse)
+    app.get('/redirect_api/*', function(clientRequest, clientResponse){
+       proxyAPIRequest(clientRequest, clientResponse)
     })
 
     app.get('/basicAuth/login', function (clientRequest, clientResponse) {
