1af6c8d0cb290dda478f3ee144d0b7c218671394
kreuzerk <kevin.kreuzer90@icloud.com>
2017-05-20 18:26:56 +0200

    kk: Added login popup

--- public/app/components/sidenav/sidenav.controller.js
+++ public/app/components/sidenav/sidenav.controller.js
@@ -1,17 +1,28 @@
 /**
  * Created by kevinkreuzer on 16.03.17.
  */
+import loginController from '../login/login.controller'
 
 export default class SidenavController {
-  constructor () {
-    this.items = [
-      {
-        icon: 'exit_to_app',
-        title: 'With SwissPassID'
-      },
-      {
-        icon: 'exit_to_app',
-        title: 'With B2B-Account'
-      }]
-  }
+    constructor($mdDialog) {
+        this.$mdDialog = $mdDialog
+    }
+
+    showBasicAuthLogin(ev) {
+        console.log('Hier')
+        this.$mdDialog.show({
+            controller: loginController,
+            controllerAs: '$ctrl',
+            templateUrl: 'components/login/login.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+            fullscreen: false
+        })
+            .then(function (answer) {
+                console.log('You changed the mode of the mock to disabled: ', answer)
+            }, function () {
+                //Handle cancel
+            });
+    }
 }
