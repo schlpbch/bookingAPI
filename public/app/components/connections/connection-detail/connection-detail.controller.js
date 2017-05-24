63e2cae617138ca30f7e9cc46d8e951893e9adce
Oliver Hofer <git@pageagent.com>
2017-05-24 21:59:19 +0200

    added prices of trips as chips

--- public/app/components/connections/connection-detail/connection-detail.controller.js
+++ public/app/components/connections/connection-detail/connection-detail.controller.js
@@ -3,8 +3,30 @@
  */
 
 export default class ConnectionDetailController {
-  constructor (bookingStore, connectionService) {
+  constructor(bookingStore, connectionService) {
     this.bookingStore = bookingStore
     this.connectionService = connectionService
+    this.prices = [];
+    this.readonly = true;
+    this.removable =false;
+
+    this.$onChanges = function (changes) {
+      if (angular.isDefined(changes.trip.currentValue)) {
+        this.getPrices()
+      }
+    }
+  }
+
+  getPrices() {
+    let headers = this.connectionService.authService.getAuthHeader()
+    this.connectionService.$http.get('../redirect_' + this.trip._links.prices.href, { headers })
+      .then((res) => {
+
+        for (var index = 0; index < res.data.length; index++) {
+          this.prices.push(res.data[index].description + " CHF " + res.data[index].price)
+        }        
+      }, (error) => {
+        this.connectionService.errorLogService.logError(error)
+      })
   }
 }
--- public/app/components/connections/connections.service.js
+++ public/app/components/connections/connections.service.js
@@ -21,22 +21,4 @@ export default class ConnectionService {
                 this.errorLogService.logError(error)
             })
     }
-
-/*
-[{"tripId":"trip-rec-context1","segments":[{"origin":{"id":"8507000","name":"Bern","date":"2017-06-14","time":"20:04"},"destination":{"id":"8507100","name":"Thun","date":"2017-06-14","time":"20:22"}}],"_links":{"self":{"href":"trips/trip-rec-context1"},"prices":{"title":"Request the cheapest prices for a trip.","href":"api/prices/trip-rec-context1"},"offers":{"title":"Request offers for a trip.","href":"api/offers?originId=8507000&destinationId=8503000&date=2017-06-14&time=20:04"},"link2WebshopB2C":{"title":"Link into Webshop B2C.","href":"https://www.sbb.ch/en/buying/pages/fahrplan/fahrplan.xhtml?suche=true&ab=tr…PreviewMode=active&von=Thun&nach=Bern&datum=Sun%2C+14.06.2017&zeit=20%3A04"},"link2MobileB2C":{"title":"Link into Mobile App B2C.","href":"https://apptest.sbbmobile.ch/trip/offer?recon=T$A=1@O=Bern@L=8507000@a=128@$A=1@O=Thun@L=8507100@a=128@$201706142004$201706142156$&date=2017-06-14"}}},{"tripId":"trip-rec-context2","segments":[{"origin":{"id":"8507000","name":"Bern","date":"2017-06-14","time":"20:34"},"destination":{"id":"8507100","name":"Thun","date":"2017-06-14","time":"20:52"}}],"_links":{"self":{"href":"trips/trip-rec-context2"},"prices":{"title":"Request the cheapest prices for a trip.","href":"api/prices/trip-rec-context2"},"offers":{"title":"Request offers for a trip.","href":"api/offers?originId=8507000&destinationId=8503000&date=2017-06-14&time=20:34"},"deeplinkWebshopB2C":{"title":"Link into Webshop B2C.","href":"https://www.sbb.ch/en/buying/pages/fahrplan/fahrplan.xhtml?suche=true&ab=tr…PreviewMode=active&von=Thun&nach=Bern&datum=Sun%2C+14.06.2017&zeit=20%3A34"},"deeplinkMobileB2C":{"title":"Link into Mobile App B2C.","href":"https://apptest.sbbmobile.ch/trip/offer?recon=T$A=1@O=Genève@L=8501008@a=128@$A=1@O=Bern@L=8507000@a=128@$201605161012$201605161156$IR 2517$&date=2017-06-14"}}}]
-*/
-    getPrice(trip) {
-        let headers = this.authService.getAuthHeader()
-        return trip._links.prices.href
-        
-        /*
-        this.$http.get('../redirect_' + trip._links.prices.href, {headers})
-        .then((res) => {
-                 // debug
-                console.log('getPrice ' + JSON.stringify(res.data))
-        }, (error) => {
-                this.errorLogService.logError(error)
-        })
-        */
-    }
 }
