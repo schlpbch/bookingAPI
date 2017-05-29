ffedce918db49e6b5f7da204ddaef95e5d1952f8
kreuzerk <kevin.kreuzer90@icloud.com>
2017-05-29 07:29:50 +0200

    kk: Rename connections to trips

--- public/app/components/trips/trips.controller.js
+++ public/app/components/trips/trips.controller.js
@@ -2,7 +2,7 @@
  * Created by kevinkreuzer on 20.03.17.
  */
 
-export default class VerbindungenController {
+export default class TripController {
   constructor (bookingStore, connectionService) {
     this.bookingStore = bookingStore
     this.connectionService = connectionService
