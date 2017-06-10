a4686a010348a0ebefb9d6bd9d961106f86fb0e0
Andreas Schlapbach <schlpbch@gmail.com>
2017-06-10 18:20:39 +0200

    Simplyfing the prices API.

--- public/app/components/trips/trip-detail/trip-detail.controller.js
+++ public/app/components/trips/trip-detail/trip-detail.controller.js
@@ -3,31 +3,35 @@
  */
 
 export default class ConnectionDetailController {
-  constructor(bookingStore, connectionService) {
+  constructor (bookingStore, connectionService) {
     this.bookingStore = bookingStore
     this.connectionService = connectionService
-    this.prices = [];
-    this.readonly = true;
-    this.removable = false;
-    this.loadingPrices = true;
+    this.prices = []
+    this.readonly = true
+    this.removable = false
+    this.loadingPrices = true
 
     this.$onChanges = function (changes) {
       if (angular.isDefined(changes.trip.currentValue)) {
         this.getPrices()
       }
     }
-    
   }
 
-  getPrices() {
+  getPrices () {
     let headers = this.connectionService.authService.getAuthHeader()
-    this.connectionService.$http.get('../redirect_' + this.trip._links.prices.href, { headers })
+    let url = '../redirect_' + this.trip._links.prices.href
+    this.connectionService.$http.get(url, {
+      headers
+    })
       .then((res) => {
-
-        for (var index = 0; index < res.data.length; index++) {
-          this.prices.push(res.data[index].description + " CHF " + res.data[index].price)
+        let price = 0
+        if (res.data.superSaverPrices === undefined) {
+          price = res.data.normalPrices[0]
+        } else {
+          price = res.data.superSaverPrices[0]
         }
-
+        this.prices.push(price / 100 + ' CHF')
         this.loadingPrices = false
       }, (error) => {
         this.connectionService.errorLogService.logError(error)
