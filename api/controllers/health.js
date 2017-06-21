846c0f4f85173d28132b4125ea5af128db4cdf2e
Andreas Schlapbach <schlpbch@gmail.com>
2017-06-21 22:47:49 +0200

    Consistently update to version 0.0.14

--- api/controllers/health.js
+++ api/controllers/health.js
@@ -6,7 +6,9 @@ module.exports = {
 
 function status (req, res) {
   let now = new Date()
-  // TODO: distinguish between the version of the API and its implementation
-  let version = '0.0.12'
+  // TODO:
+  //  - return a real JSON object
+  //  - distinguish between the version of the API and its implementation
+  let version = '0.0.14'
   res.json("{ status: 'ok', date: '" + now + "', version: '" + version + "'}")
 }
