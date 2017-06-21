e1fc70c10d9f9eba35322ea71544364a1be4701b
Andreas Schlapbach <schlpbch@gmail.com>
2017-06-21 22:44:56 +0200

    Using dateformat for date and time

--- public/app/components/travel-wish/travel-wish.service.js
+++ public/app/components/travel-wish/travel-wish.service.js
@@ -1,39 +1,31 @@
-/**
- * Created by kevinkreuzer on 20.03.17.
- */
+'use strict'
+
+var dateFormat = require('dateformat')
 
 export default class ReiseWunschService {
-    constructor($http, bookingStore, tabService, errorLogService, authService) {
-        this.$http = $http
-        this.bookingStore = bookingStore
-        this.tabService = tabService
-        this.errorLogService = errorLogService
-        this.authService = authService
-    }
+  constructor ($http, bookingStore, tabService, errorLogService, authService) {
+    this.$http = $http
+    this.bookingStore = bookingStore
+    this.tabService = tabService
+    this.errorLogService = errorLogService
+    this.authService = authService
+  }
 
-    getTrips(originId, destinationId, isArrival, date, time) {
-        let headers = this.authService.getAuthHeader()
+  getTrips (originId, destinationId, isArrival, date, time) {
+    let headers = this.authService.getAuthHeader()
 
-        var today = new Date();
-        var yyyy = today.getFullYear();
-        var dd = today.getDate();
-        var mm = today.getMonth()+1;
+    let now = new Date()
+    let isoDate = dateFormat(now, `isoDate`)
+    let isoTime = dateFormat(now, `isoTime`)
 
-        if(dd<10) {
-          dd='0'+dd;
-        }
-        if(mm<10){
-          mm='0'+mm;
-        }
-        var today = yyyy + '-' + mm + '-' + dd;
-
-        this.$http.get(`../redirect_api/trips?originId=${originId.value}&destinationId=${destinationId.value}&date=${today}&time=19:00`,
-            {headers})
-            .then((res) => {
-                this.bookingStore.trips = res.data
-                this.tabService.goToNextTab()
-            }, (error) => {
-                this.errorLogService.logError(error)
-            })
-    }
+    this.$http.get(`../redirect_api/trips?originId=${originId.value}&destinationId=${destinationId.value}&date=${isoDate}$&time=${isoTime}`, {
+      headers
+    })
+      .then((res) => {
+        this.bookingStore.trips = res.data
+        this.tabService.goToNextTab()
+      }, (error) => {
+        this.errorLogService.logError(error)
+      })
+  }
 }
